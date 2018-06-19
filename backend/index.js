9191257f790bff1ce1ed6ec6613431afb14455b9
Ahmed elSayed <aelsayed.mo@gmail.com>
2018-06-19 02:24:19 +0200

    application configuration.

--- backend/index.js
+++
@@ -1,9 +0,0 @@
-const axios = require("axios");
-
-axios.get('https://graph.facebook.com/oauth/access_token?client_id=184423675597370&client_secret=1e9ac56f4eda0e9e5cdd23d98c83de42&grant_type=client_credentials')
-  .then(function (response) {
-    console.log(response.data.access_token);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
